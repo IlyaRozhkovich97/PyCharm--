b86c7ec303dd3ae36826da3f482c480eef1fac84
Ilya <rozhkovich.ilya97@gmail.com>
2023-12-08 22:41:02 +0300

    Initial 4

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from funcs import add_numbers
+from funcs import subtract_numbers
+
 
 result = add_numbers(5, 10)
 print("Результат сложения:", result)
+
+
+result = subtract_numbers(15, 7)
+print("Результат вычитания:", result)
